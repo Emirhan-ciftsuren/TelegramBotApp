2ced87e10643df02585fd897ccb11b344419558a
Emirhan <emircftsrn@gmail.com>
2026-07-22 09:34:51 +0300

    feat: add DTOs

---
+++ TelegramBotApp/TelegramBotApp.Application/DTOs/CreateNotificationDto.cs
@@ -0,0 +1,7 @@
+﻿namespace TelegramBotApp.Application.DTOs;
+
+public class CreateNotificationDto
+{
+    public long ChatId { get; set; } // Telegram'daki hedef sohbet/kullanıcı ID'si
+    public string Message { get; set; } = string.Empty;
+}
---
+++ TelegramBotApp/TelegramBotApp.Application/DTOs/TelegramUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TelegramBotApp.Application.DTOs;
+
+public class TelegramUserDto
+{
+    public long TelegramUserId { get; set; }
+    public string? Username { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string? LastName { get; set; }
+    public long ChatId { get; set; }
+}
